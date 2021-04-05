5f2c8afea58ae5d9f4f2309c8823c7802b0888ec
Matheus Carneiro <mchcar@vivaldi.net>
2021-04-05 16:55:17 -0400

    Primeira classe: Position

--- src/application/App.java
+++ src/application/App.java
@@ -3,10 +3,14 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import boardgame.Position;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.US);
         Scanner input = new Scanner(System.in);
+
+        Position pos = new Position(row, column);
 
         input.close();
     }
